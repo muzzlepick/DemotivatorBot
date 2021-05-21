e1d72166cb6242bed05e410bed28b274a9d271ab
Глеб Климков <klimkovg13@gmail.com>
2021-05-21 23:08:33 +0300

    Update

---
+++ main.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher, executor, types
+import photo_editor
+
+TOKEN = ''
+
+try:
+    bot = Bot(token= TOKEN)
+    dp = Dispatcher(bot)
+
+    @dp.message_handler(commands=['start'])
+    async def start(message: types.Message):
+        await bot.send_message(message.from_user.id, 'Отправь мне фото с подписью')
+
+    @dp.message_handler(content_types=['photo'])
+    async def photo(message):
+
+        await message.photo[-1].download('pic/mem.jpg')      
+
+        if message.caption:
+            text = message.caption
+
+            photo_editor.add_text(text)    
+            await bot.send_photo(message.from_user.id, photo=open('pic/paste/ok.jpg', 'rb'))
+        else:
+            await bot.send_message(message.from_user.id, 'Ты забыл добавить подпись')
+
+    if __name__ == '__main__':
+        print('Бот успешно запущен')
+        executor.start_polling(dp, skip_updates=True)
+except:
+    print('Вы забыли ввести токен\nЭто можно сделать в файле main.py')
---
+++ photo_editor.py
@@ -0,0 +1,55 @@
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+'''LOGIC FOR DEMOTIVATOR'''
+def add_text(text):
+    template = Image.open('pic/template.jpg')
+    mem = Image.open('pic/mem.jpg').convert('RGBA')
+
+    width = 610
+    height = 569
+    resized_mem = mem.resize((width, height), Image.ANTIALIAS)
+
+    text_position = (0, 0)
+    text_color = (266,0,0)
+
+
+    strip_width, strip_height = 700, 1300
+
+    def findLen(text_len):
+        counter = 0    
+        for i in text_len:
+            counter += 1
+        return counter
+
+    font_width = 60
+    
+    if findLen(text) >= 25:
+        font_width = 50
+
+    background = Image.new('RGB', (strip_width, strip_height)) #creating the black strip
+    draw = ImageDraw.Draw(template)
+
+    if '\n' in text:  
+        split_offers = text.split('\n')
+
+        for i in range(2):
+            if i == 1:
+                strip_height += 110
+                font_width -= 20
+            font = ImageFont.truetype("fonts/font.ttf", font_width) 
+            text_width, text_height = draw.textsize(split_offers[i], font)
+
+            position = ((strip_width-text_width)/2,(strip_height-text_height)/2)
+            draw.text(position, split_offers[i], font=font)
+    else:
+        font = ImageFont.truetype("fonts/font.ttf", font_width)
+        text_width, text_height = draw.textsize(text, font)
+        strip_height = 1330
+        position = ((strip_width-text_width)/2,(strip_height-text_height)/2)
+        draw.text(position, text, font=font)
+
+    template.paste(resized_mem, (54, 32),  resized_mem)
+    template.save("pic/paste/ok.jpg")
+        
